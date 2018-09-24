bde7b7954d738645f5d0964f42ba17d732e77975
yuzuriha <wellmagg@gmail.com>
2018-09-24 16:13:44 +0800

    把要上傳的資訊存到資料庫

--- model/newMethod.php
+++ model/newMethod.php
@@ -19,6 +19,9 @@ if(file_exists(iconv("utf-8", "big5", $uploadfile)))
   		echo "<script>location.href='index.php?item=NewMethod';</script>";
 	}
 }
-
+$praetor = new Praetor();
+	print_r($_POST['max_file_size']);
+	$praetor->custoinsert('algorithm', array('AID'=>mt_rand(0, 10000), 'OwnerPID'=>$_SESSION['user']['PID'], 'AlgorithmTitle'=>$_POST['titleInfo'],'GitHub'=>$_POST['github'],'PdfFileName'=>$_FILES['myfile']['name'], 'Logo'=>'logo.png','StimulusType'=>'int', 'PortNumber'=>'1137','ServerIP'=>$_POST['serverInfo']));
+	echo "<script>location.href='index.php?item=Login';</script>";
 
 ?>
