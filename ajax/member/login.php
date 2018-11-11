ac4303af97b4c5e1144eb4258d556d71a74dac6a
yuzuriha <wellmagg@gmail.com>
2018-11-11 20:20:39 +0800

    完成出版的register

--- ajax/member/login.php
+++ ajax/member/login.php
@@ -3,7 +3,7 @@
 	include('../../model/ajax_model.php');
 	$praetor = new Praetor();
 	$sql = "SELECT * FROM pmanager WHERE PID=%d_PID AND Password =%s_Password";
-	$data = $praetor->custosql($sql, array('PID'=>$_POST['name'], 'Password'=>$_POST['password']));
+	$data = $praetor->custosql($sql, array('PID'=>$_POST['PhoneNumber'], 'Password'=>$_POST['password']));
 	
 	if($data[0] != ""){
 		$_SESSION['user'] = $data[0];
