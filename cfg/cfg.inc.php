6e2ff935550d2d1cf6d89afed83bab3172908593
hsiang <wellmagg@gmail.com>
2018-11-13 22:42:55 +0800

    apply server changes

--- cfg/cfg.inc.php
+++ cfg/cfg.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 /************* 資料庫定義  ****************/
-define ('SYSTEM_DBHOST','localhost'); 
+define ('SYSTEM_DBHOST','127.0.0.1'); 
 define ('SYSTEM_DBNAME_FISHER','fisher');
 define ('SYSTEM_DBNAME_PRAETOR','praetor');
 define ('SYSTEM_DBUSER','root');
-define ('SYSTEM_DBPWD','root');
+define ('SYSTEM_DBPWD','pwd');
 ?>
